3c30937194e91310157639f7532d593c13c094c4
Giang N <hngnaig@gmail.com>
2017-09-22 10:27:30 +0700

    Add error_chain

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 #[macro_use] extern crate diesel;
 #[macro_use] extern crate diesel_codegen;
+#[macro_use] extern crate error_chain;
 extern crate dotenv;
 extern crate serde;
 extern crate serde_json;
+extern crate url;
 #[macro_use] extern crate serde_derive;
 
 use diesel::prelude::*;
@@ -12,9 +14,10 @@ use std::env;
 
 pub mod schema;
 pub mod models;
+pub mod errors;
 
 pub fn establish_connection() -> PgConnection {
     dotenv().ok();
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
+    let database_url = env::var("DATABASE_URL").chain_err("DATABASE_URL must be set");
+    PgConnection::establish(&database_url).chain_err(&format!("Error connecting to {}", database_url))
 }
