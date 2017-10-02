729686a14dd5562540276cb19c59ae453d3c69b3
Pierre-Henri Symoneaux <pierre-henri.symoneaux@nokia.com>
2017-10-02 20:40:42 +0200

    Added unit test helpers module

--- src/helpers.rs
+++ src/helpers.rs
@@ -20,3 +20,32 @@ pub fn digest(input: &str) -> String {
 pub fn epoch_now() -> u64 {
     SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs()
 }
+
+#[cfg(test)]
+mod tests {
+    use std::thread::sleep;
+    use std::time::Duration;
+
+    #[test]
+    fn digest() {
+        assert_eq!(super::digest("foobar"), "c3ab8ff13720e8ad9047dd39466b3c8974e592c2fa383d4a3960714caef0c4f2".to_string());
+        assert_eq!(super::digest(""), "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855".to_string());
+    }
+
+    #[test]
+    fn epoch_now() {
+        let first = super::epoch_now();
+        sleep(Duration::new(1, 0));
+        let second = super::epoch_now();
+        assert!(second >= first + 1);
+    }
+
+    #[test]
+    fn gen_random_hash() {
+        let first = super::gen_random_hash();
+        let second = super::gen_random_hash();
+        assert!(first != second);
+        assert_eq!(first.len(), 64);
+        assert_eq!(second.len(), 64);
+    }
+}
