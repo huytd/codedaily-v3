dd31292b41c71162b71e886199a13dc9a7e9cb07
Matthias Devlamynck <matthias.devlamynck@mailoo.org>
2017-10-26 20:47:43 +0200

    Implement backend of comments system (#30)
    * Implement posting comments about a link (a.k.a. post) and querying comments of a link with pagination
    
    * Fix migration sql and rename link to link_id
    
    * Revert link_id to link

---
+++ migrations/20171007182038_create_comments/up.sql
@@ -0,0 +1,6 @@
+CREATE TABLE comments (
+  id SERIAL PRIMARY KEY,
+  message VARCHAR NOT NULL,
+  link_id INTEGER NOT NULL REFERENCES links (id),
+  author_id INTEGER NOT NULL REFERENCES users (id)
+)
